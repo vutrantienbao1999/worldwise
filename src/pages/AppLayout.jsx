26675cebcac5a4622e8ac43d0c08735e762bf23f
Vu Tran Tien Bao <vutrantienbao@gmail.com>
2025-03-23 15:49:03 +0700

    first commit

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App Nav</p>
+    </div>
+  );
+}
+
+export default AppLayout;
